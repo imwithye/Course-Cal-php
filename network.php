8223c3203e40c2187b67772f742a347fcfd568d4
Ciel <imwithye@gmail.com>
2013-10-06 15:19:14 +0800

    network.php; courseURL(code) and timetableURL functions added. @Ciel

---
+++ network.php
@@ -0,0 +1,32 @@
+<?php
+    /*
+     * author Ciel
+     */
+    function courseURL($code){
+        $y = date("Y");
+        $m = date("m");
+        $sem = 0;
+        if($m<5){
+            $y -= 1;
+            $sem = 2;
+        }else if($m>=11){
+            $sem = 2;
+        }else{
+            $sem = 1;
+        }
+        $url = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.";
+        $url .= "main_display1?staff_access=false&acadsem=".$y.";".$sem;
+        $url .= "&r_subj_code=".strtoupper($code)."&boption=Search&r_search_type=F";
+        return $url;
+    }
+    
+    function timetableURL(){
+        $y = date("Y");
+        $m = date("m");
+        if($m<=5)
+            $y = $y-1;
+        $url = "http://www.ntu.edu.sg/Students/Undergraduate/AcademicServices/AcademicCalendar/Pages/";
+        $url .= $y."-".($y%100+1).".aspx";
+        return $url;
+    }
+?>
