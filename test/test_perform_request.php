b97d3881edc5db945940831ca096a3eb505e4fba
Ciel <imwithye@gmail.com>
2013-10-08 11:16:15 +0800

    test receiving url from _request. @Ciel

---
+++ test/test_perform_request.php
@@ -0,0 +1,10 @@
+<?php
+    require_once 'Course.Class.php';
+    $url = $_REQUEST['url'];
+    $courses = analysePrintablePage($url);
+    echo 'Testing receive url from _REQUEST</br>';
+    foreach($courses as $course){
+        echo $course->toString();
+        echo '</br></br>';
+    }
+?>
