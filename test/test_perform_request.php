ba4835fc05405f4e38f6e34a0c7b0684062baee9
Ciel <imwithye@gmail.com>
2013-10-10 00:16:00 +0800

    1. delete useless files 2. bug fixed for test file

--- test/test_perform_request.php
+++ test/test_perform_request.php
@@ -1,7 +1,8 @@
 <?php
     include_once '../Course.Class.php';
     $url = $_REQUEST['url'];
-    $courses = analysePrintablePage($url);
+    $info = getUserInfo($url);
+    $courses = analysePrintablePage($url, $info);
     echo 'Testing receive url from _REQUEST</br>';
     foreach($courses as $course){
         echo $course->toString();
