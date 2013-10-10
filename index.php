ba2807e64f59db93e6cfd6ce6ca65c54ec3ce87c
Ciel <imwithye@gmail.com>
2013-10-10 23:32:20 +0800

    index page, redirect to http://imwithye.github.io/course

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-?>
+$secone = 1;
+$url = "http://imwithye.github.io/course";
+?> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Course Cal</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <?php echo '<meta http-equiv="refresh" content="'.$secone.'; url='.$url.'">'; ?>
+        <!-- stylesheet -->
+        <link href="stylesheet/main.css" rel="stylesheet">
+    </head>
+    <body>
+    </body>
+</html>
