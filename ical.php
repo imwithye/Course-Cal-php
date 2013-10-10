09504f0b76760c1eda759b548395b82e8a0e76a9
Ciel <imwithye@gmail.com>
2013-10-10 17:13:51 +0800

    1. ical.php update 2. a test file for ical. Test creating and downloading;

--- ical.php
+++ ical.php
@@ -3,22 +3,8 @@
     require_once 'network.php';
     require_once 'Course.Class.php';
     
-    function semInfo(){
-        return array('2013' => array('1' => 120813
-                                        ,'2' => 130114)
-                    , '2014' => array('1' => 110814
-                                    , '2' => 120115)
-                    , '2015' => array('1' => 100815
-                                    , '2' => 110116)
-                    , '2016' => array('1' => 080816
-                                    , '2' => 090117));
-    }
-    
-    function setCourseEvent($course, $event, $SemTime){
-        $startTime = array('year' => $SemTime%100+2000
-                        , 'month' => ($SemTime/100)%100
-                        , 'day' => $SemTime%10000);
-        //TO DO;
+    function setCourseEvent(Course $course, vcalendar $ical, $info){
+        $startTime = semInfo($info['year'], $info['sem']);
         return $startTime;  //This is for testing.
     }
     
@@ -27,18 +13,16 @@
         $info = getUserInfo($url);
         if($info==null)
             return null;
-        $SemInfo = semInfo();
         $courses = analysePrintablePage($url, $info);
         if(count($courses)==0)
             return null;
         $config = array('unique_id' => $info['p1']
-                    , 'TZID' => $tz);
-        $v = new vcalendar($config);
+                    , 'TZID' => $tz
+                    , 'filename' => $info['p1']);
+        $ical = new vcalendar($config);
         foreach($courses as $course){
-            $event = & $v->newComponent('vevent');
-            setCourseEvent($course, $event, $SemInfo[$info['year']['sem']]);
+            setCourseEvent($course, $ical, $info);
         }
-        return $v;
+        return $ical;
     }
-   
 ?>
