f8f4098b57fa6848ef9fc6921724d88f45c8ce6f
Ciel <imwithye@gmail.com>
2013-10-11 11:01:40 +0800

    rewrite url jump code in index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<?php
-$secone = 1;
-$url = "http://imwithye.github.io/course";
-?> 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Course Cal</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <?php echo '<meta http-equiv="refresh" content="'.$secone.'; url='.$url.'">'; ?>
-        <!-- stylesheet -->
-        <link href="stylesheet/main.css" rel="stylesheet">
-    </head>
-    <body>
-    </body>
-</html>
+<?php 
+    header("Location: http://imwithye.github.io/course");
+    exit;
+?>
