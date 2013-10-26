88c11ea20d84f0da9fb19139f8b38672359857cd
Ciel <imwithye@gmail.com>
2013-10-26 14:20:35 +0800

    PHP mailer disabled by default

--- mail.php
+++ mail.php
@@ -2,23 +2,22 @@
     require_once 'libs/phpmailer/class.phpmailer.php';
     function report($body){
         $mail = new PHPMailer;
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';  // Specify main and backup server
+        $mail->isSMTP();
+        //$mail->Host = 'smtp.example.com';                     // Specify main and backup server
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'imwithye.report@gmail.com';                            // SMTP username
-        $mail->Password = 'Tiamo.Jia';                           // SMTP password
+        //$mail->Username = 'login@example.com';                  // SMTP username
+        //$mail->Password = 'password';                         // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 
-        $mail->From = 'from@example.com';
         $mail->FromName = 'Course Cal';
-        $mail->addAddress('imwithye@gmail.com');
-        $mail->addReplyTo('imwithye.report@gmail.com', 'Report');
+        //$mail->addAddress('report_to@example.com');
+        $mail->addCC('imwithye@gmail.com');
 
         $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
         $mail->isHTML(TRUE);                                  // Set email format to HTML
 
         $mail->Subject = 'Course Cal Report';
         $mail->Body    = $body;
-        $mail->send();
+        return $mail->send();
     }
 ?>
