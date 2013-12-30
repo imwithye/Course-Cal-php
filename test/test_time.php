e6629d0c305be9cc5df243208d971847ab3ab29d
Ciel <imwithye@gmail.com>
2013-12-31 01:06:36 +0800

    refactor time.php, rewrite test case

--- test/test_time.php
+++ test/test_time.php
@@ -1,12 +1,42 @@
 <?php
-    include_once '../time.php';
-    echo 'Test semInfo</br>';
-    $date1 = semInfo(2013, 1);
-    echo $date1['day'].'/'.$date1['month'].'/'.$date1['year'];
-    echo '</br><hr>';
-    
-    echo 'Testing fewDaysNextOrBefore.</br>';
-    $date = array('year'=>2013, 'month'=>12,'day'=>1);
-    $date2 = fewDaysNextOrBefore($date, '+1 days');
-    echo $date2['day'].'/'.$date2['month'].'/'.$date2['year'];
+	require_once '../time.php';
+	
+	echo 'Test Event Time Class: </br>';
+	$time = array('year' => '1995'
+			, 'month' => '02'
+			, 'day' => '28'
+			, 'startTime' => '0830'
+			, 'endTime' => '0930'
+			);
+	$eventTime = new EventTime($time);
+	echo 'Year: '.$eventTime->year.'</br>';
+	echo $eventTime->toString();
+	
+	echo '</br></br>';
+	echo 'Test Lesson Time Class: </br>';
+	$time = array(
+			'startTime' => '0830'
+			, 'endTime' => '0930'
+			, 'wkDay' => '01'
+			);
+	$lessonTime = new LessonTime($time);
+	echo 'Year: '.$lessonTime->year.'</br>';
+	echo 'wkDay: '.$lessonTime->wkDay.'</br>';
+	echo $lessonTime->toString();
+	
+	echo '</br></br>';
+	echo 'Test semInfo: </br>';
+	$sem = semInfo('2013','1');
+	echo 'Year: '.$sem['year'].'</br>';
+	echo 'Month: '.$sem['month'].'</br>';
+	echo 'Day: '.$sem['day'].'</br>';
+	
+	echo '</br></br>';
+	echo 'Test fewDaysNextOrBefore: </br>';
+	$time = array('year' => '1995'
+			, 'month' => '02'
+			, 'day' => '28'
+			);
+	$newTime = fewDaysNextOrBefore($time, '-1 year');
+	echo 'Year: ', $newTime['year'];
 ?>
--- time.php
+++ time.php
@@ -1,116 +1,130 @@
 <?php  
-    class Time{
-        protected $year;
-        protected $month;
-        protected $day;
+	class EventTime{
+		protected $year;
+		protected $month;
+		protected $day;
+		protected $startTime;
+		protected $endTime;
         
-        public function __construct($year, $month, $day) {
-            $this->year = $year;
-            $this->month = $month;
-            $this->day = $day;
-        }
-                       
-        public function getYear(){
-            return $this->year;
-        }
+		public function __construct(array $time) {
+			$this->year = intval($time['year']);
+			$this->month = intval($time['month']);
+			$this->day = intval($time['day']);
+			$this->startTime = intval($time['startTime']);
+			$this->endTime = intval($time['endTime']);
+		}//function __construct(array $time);
+		
+		public function __get($property_name) {
+			return isset($this->$property_name) ? $this->$property_name : null;
+		}//function __get($property_name);
+		
+		public function toString(){
+			return $this->day.'/'.$this->month.'/'.$this->year.' '.$this->startTime.'-'.$this->endTime;
+		}//function toString();
+	}//class EventTime;
+	
+	class LessonTime extends EventTime{
+		private $wkDay;
+		private $wkRepeat;
+		
+		public function __construct(array $time){
+			$this->year = array_key_exists('year', $time) ? intval($time['year']) : 0;
+			$this->month = array_key_exists('month', $time) ? intval($time['month']) : 0;
+			$this->day = array_key_exists('day', $time) ? intval($time['day']) : 0;
+			$this->startTime = intval($time['startTime']);
+			$this->endTime = intval($time['endTime']);
+			$this->wkDay = $time['wkDay'];
+			$this->wkRepeat = array(FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE);
+		}//function __construct(array $time);
         
-        public function getMonth(){
-            return $this->month;
-        }
-        
-        public function getDay(){
-            return $this->day;
-        }
-        
-        public function toString(){
-            return $this->day.'/'.$this->month.'/'.$this->year;
-        }
-    }
+		public function __get($property_name) {
+			return isset($this->$property_name) ? $this->$property_name : null;
+		}//function __get($property_name);
+		
+		public function toString(){
+			$string = "Starting at ".$this->startTime." Ending at ".$this->endTime." on ".$this->wkDay." with Repeat: </br>";
+			foreach ($this->wkRepeat as $wk){
+				if($wk)
+					$string .= 'TRUE ';
+				else
+					$string .= 'FALSE ';
+			}
+			$string .= '</br>';
+			return $string;
+		}//function toString();
+	}//class LessonTime;
+	
+	function semInfo($year, $sem){
+		$data = array('2013' => array('1' => 120813
+									,'2' => 130114)
+					, '2014' => array('1' => 110814
+									, '2' => 120115)
+					, '2015' => array('1' => 100815
+									, '2' => 110116)
+					, '2016' => array('1' => 080816
+									, '2' => 090117));
+		$SemTime = $data[strval($year)][strval($sem)];
+		return array('year' => intval($SemTime%100+2000)
+				, 'month' => intval(($SemTime/100)%100)
+				, 'day' => intval($SemTime/10000));
+	}//semInfo($year, $sem);
     
-    class EventTime extends Time{
-        protected $startTime;
-        protected $endTime;
-        
-        public function getStartTime(){
-            return $this->startTime;
-        }
-        
-        public function getEndTime(){
-            return $this->endTime;
-        }
-    }
+	function month($month){
+		if($month=='jan')
+			return 1;
+		else if($month=='feb')
+			return 2;
+		else if($month=='mar')
+			return 3;
+		else if($month=='apr')
+			return 4;
+		else if($month=='may')
+			return 5;
+		else if($month=='jun')
+			return 6;
+		else if($month=='jul')
+			return 7;
+		else if($month=='aug')
+			return 8;
+		else if($month=='sep')
+			return 9;
+		else if($month=='oct')
+			return 10;
+		else if($month=='nov')
+			return 11;
+		else if($month=='dec')
+			return 12;
+		else
+			return 0;
+	}//month($month);
     
-    function semInfo($year, $sem){
-        $data = array('2013' => array('1' => 120813
-                                        ,'2' => 130114)
-                    , '2014' => array('1' => 110814
-                                    , '2' => 120115)
-                    , '2015' => array('1' => 100815
-                                    , '2' => 110116)
-                    , '2016' => array('1' => 080816
-                                    , '2' => 090117));
-        $SemTime = $data[strval($year)][strval($sem)];
-        return array('year' => intval($SemTime%100+2000)
-                        , 'month' => intval(($SemTime/100)%100)
-                        , 'day' => intval($SemTime/10000));
-    }
+	function week($day){
+		if($day=='sun')
+			return 7;
+		else if($day=='mon')
+			return 1;
+		else if($day=='tue')
+			return 2;
+		else if($day=='wed')
+			return 3;
+		else if($day=='thu')
+			return 4;
+		else if($day=='fri')
+			return 5;
+		else if($day=='sat')
+			return 6;
+		else
+			return 0;
+	}//week($day);
     
-    function month($month){
-        if($month=='jan')
-            return 1;
-        else if($month=='feb')
-            return 2;
-        else if($month=='mar')
-            return 3;
-        else if($month=='apr')
-            return 4;
-        else if($month=='may')
-            return 5;
-        else if($month=='jun')
-            return 6;
-        else if($month=='jul')
-            return 7;
-        else if($month=='aug')
-            return 8;
-        else if($month=='sep')
-            return 9;
-        else if($month=='oct')
-            return 10;
-        else if($month=='nov')
-            return 11;
-        else if($month=='dec')
-            return 12;
-        else
-            return 0;
-    }
-    
-    function week($day){
-        if($day=='sun')
-            return 7;
-        else if($day=='mon')
-            return 1;
-        else if($day=='tue')
-            return 2;
-        else if($day=='wed')
-            return 3;
-        else if($day=='thu')
-            return 4;
-        else if($day=='fri')
-            return 5;
-        else if($day=='sat')
-            return 6;
-        else
-            return 0;
-    }
-    
-    function fewDaysNextOrBefore($time, $n){
-        $year = $time['year'];
-        $month = $time['month'];
-        $day = $time['day'];
-        $Date = strtotime($year.'-'.$month.'-'.$day);
-        $newDate = explode('/',date('n/j/Y', strtotime($n, $Date)));
-        return array('year' => intval($newDate[2])
-                    ,'month' => intval($newDate[0])
-                    ,'day' => intval($newDate[1]));
+	function fewDaysNextOrBefore(array $time, $n){
+		$year = array_key_exists('year', $time) ? intval($time['year']) : 0;
+		$month = array_key_exists('month', $time) ? intval($time['month']) : 0;
+		$day = array_key_exists('day', $time) ? intval($time['day']) : 0;
+		$Date = strtotime($year.'-'.$month.'-'.$day);
+		$newDate = explode('/',date('n/j/Y', strtotime($n, $Date)));
+		return array('year' => intval($newDate[2])
+					,'month' => intval($newDate[0])
+					,'day' => intval($newDate[1]));
     }
 ?>
