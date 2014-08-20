112048899e28ff51e314155d32a84ea41d10df42
Ciel <imwithye@gmail.com>
2014-08-20 18:20:46 +0800

    accept json only

--- perform.php
+++ perform.php
@@ -1,6 +1,5 @@
 <?php
 	require_once 'ical.php';
-	require_once 'mail.php';
 	
 	$json_data = array_key_exists('json', $_REQUEST) ? $_REQUEST['json'] : null;
 	if(!$json_data){
@@ -12,31 +11,13 @@
 		echo 'Error! #wrong json format.';
 		exit;
 	}
-	
-	$url = array_key_exists('url', $info) ? $info['url'] : null;
-	if($url){
-		$result = createCalWithPrintablePage($url);
-		if(!$result) {
-			echo 'Error! #wrong url';
-			exit;
-		}
-		else{
-			if($result['error']) {
-				report('Error is caught in this url: '.$result['url']); //report error
-			}
-			$result['ics']->returnCalendar();
-			exit;
-		}
+	$result = createCalWithCustomInformation($info);
+	if($result) {
+		$result['ics']->returnCalendar();
+		exit;
 	}
 	else {
-		$result = createCalWithCustomInformation($info);
-		if($result) {
-			$result['ics']->returnCalendar();
-			exit;
-		}
-		else {
-			echo 'Error! #cannot create ics with given json.';
-			exit;
-		}
+		echo 'Error! #cannot create ics with given json.';
+		exit;
 	}
 ?>
